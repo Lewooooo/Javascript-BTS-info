6cb3cd2b8d25c17c03512929129ece3840846cb8
lewooo <l.leo62118@gmail.com>
2025-09-11 11:44:55 +0200

    nettoyage du code

--- js/script.js
+++ js/script.js
@@ -9,34 +9,23 @@ function monSpanClick(){
     this.classList.toggle('SpanBlack')
 }
 
-let lesh2 = document.getElementsByTagName("h2");
-for (let unh2 of lesh2){
-    unh2.classList.add("cacher");
-}
-
 let monbouton = document.getElementById("monbouton");
 monbouton.addEventListener("click", monboutonclick)
+
+let lesh2 = document.getElementsByTagName("h2");
 function monboutonclick() {
     for (let unh2 of lesh2){
         unh2.classList.toggle("cacher");
     }
 }
-monbouton.textContent="Afficher/Cacher"
 
 let max = 10;
 let min = 1;
 
 
 
-
-
 function getRandomArbitrary(min, max) {
     return Math.random() * (10 - 1) + 1;
-}
-
-let lesimg = document.getElementsByTagName("img");
-for (let unimg of lesimg){
-    unimg.classList.add("cacher");
 }
 
 let reponse = Math.round(getRandomArbitrary());
