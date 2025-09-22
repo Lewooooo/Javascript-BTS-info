10cba2b31ea89123ee9e4c341bd904fdccbf7e45
Mathis <pochetmathis@gmail.com>
2025-09-22 09:24:07 +0200

    ok ?

--- js/carousel.js
+++ js/carousel.js
@@ -1,8 +1,10 @@
-let indiceCourant=0;
-const carrousels=document.getElementsByClassName("carrousel");
+let indiceCourant = 0;
+const carrousels = document.getElementsByClassName("carrousel");
 
-for (i=1;i<carrousels.children.length;i++) {
-    let li=carrousels.children[i];
-    li.style.display="none";
+if (carrousels.length > 0) {
+    let carrou1 = carrousels[0];
+    for (i = 1; i < carrou1.children.length; i++) {
+        let li = carrou1.children[i];
+        li.style.display = "none";
+    }
 }
-
