ec6b2d711f605b9c72cf6e6f6df713b877557d21
Leo <l.leo62118@gmail.com>
2025-09-29 14:24:46 +0200

    oui

--- js/mario.js
+++ js/mario.js
@@ -3,5 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const liste = document.getElementById('liste');
     const btSupprimer = document.getElementById('btSupprimer');
 
-    
+    let indice_gagant = Math.floor(Math.random() * liste.children.length);
+    console.log(indice_gagant);
+
+    for (let ul of liste.children) {
+        ul.addEventListener("click", () => test(ul));
+    }
+
+    function test(ul){
+        ul.children[0].classList.add("gagner");
+    }
 });
