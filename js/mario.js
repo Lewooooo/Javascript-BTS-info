595f81d9bc4c09b433a29f9e5679b2444e4c8022
Mathis <pochetmathis@gmail.com>
2025-09-29 11:58:29 +0200

    coucou les congolais coucou les congolais ding eh ding

--- js/mario.js
+++ js/mario.js
@@ -1,3 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const btAjouter = document.getElementById('btAjouter');
+    const liste = document.getElementById('liste');
+    const btSupprimer = document.getElementById('btSupprimer');
 
 });
