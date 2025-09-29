56ad29d54f247082684ed9f936329181c0cfee77
Leo <l.leo62118@gmail.com>
2025-09-29 14:45:20 +0200

    ouui

--- js/mario.js
+++ js/mario.js
@@ -6,11 +6,40 @@ document.addEventListener('DOMContentLoaded', () => {
     let indice_gagant = Math.floor(Math.random() * liste.children.length);
     console.log(indice_gagant);
 
-    for (let ul of liste.children) {
-        ul.addEventListener("click", () => test(ul));
+    for (let index = 0; index < liste.children.length; index++) {
+        let li = liste.children[index];
+        if (index == indice_gagant) {
+            li.addEventListener("click", () => gagner(li))
+        } else{
+            li.addEventListener("click", () => perdre(li))
+        }
     }
 
-    function test(ul){
-        ul.children[0].classList.add("gagner");
+    btAjouter.addEventListener("click", ajouter);
+
+    function ajouter() {
+        let li = document.createElement("li");
+        let div = document.createElement("div");
+        div.classList.add("carte");
+        li.appendChild(div);
+        liste.appendChild(li);
+        generate_random(liste.children.length);
+        console.log(indice_gagant);
+        if (liste.children.length == indice_gagant) {
+            li.addEventListener("click", () => gagner(li))
+        } else{
+            li.addEventListener("click", () => perdre(li))
+        }
+    }
+
+    function gagner(li){
+        li.children[0].classList.add("gagner");
+    }
+    function perdre(li){
+        li.children[0].classList.add("perdu");
+    }
+
+    function generate_random(length){
+        indice_gagant = Math.floor(Math.random() * length);
     }
 });
