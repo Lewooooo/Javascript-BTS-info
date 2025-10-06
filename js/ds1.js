c0aef4d87e95fe69c7f72b89678bceb1cbbc48a9
Leo <l.leo62118@gmail.com>
2025-10-06 08:51:30 +0200

    oui

--- js/ds1.js
+++ js/ds1.js
@@ -2,15 +2,18 @@ document.addEventListener("DOMContentLoaded",()=>{
     const btn_like = document.getElementById("btn-like");
     const btn_dislike = document.getElementById("btn-dislike");
 
+    const cpt_like = document.getElementById("cpt-like");
+    const cpt_dislike = document.getElementById("cpt-dislike");
+
     btn_like.addEventListener('click',like);
     btn_dislike.addEventListener('click',dislike);
     
 
     function like() {
-
+        cpt_like.textContent = Number(cpt_like.textContent) + 1;
     }
-    
-    function dislike() {
 
+    function dislike() {
+        cpt_dislike.textContent = Number(cpt_dislike.textContent) + 1;
     }
 })
