db02ab02851dab37a5c7ba16a8dd3ebdcfbd0bfb
Leo <l.leo62118@gmail.com>
2025-10-06 08:44:35 +0200

    pouce

--- js/ds1.js
+++ js/ds1.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     const btn_like = document.getElementById("btn-like");
     btn_like.addEventListener('click',like);
     
+    
 
     function like() {
 
